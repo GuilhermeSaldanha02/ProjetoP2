82039266c66d580bdb89defbb0ddf4723fb5dc10
Guilherme Saldanha <guilhermesaldanha007@gmail.com>
2025-11-18 11:44:09 -0300

    fix: Ajusta integração com API (jqXHR/text), SweetAlert2 e proteção de sessão; atualiza scripts de admin, mensagens e contato; bust de cache do api.js

--- projeto-iates/js/api.js
+++ projeto-iates/js/api.js
@@ -1,77 +1,30 @@
 function obterMensagens() {
-
-    var retorno = [];
-
-    var consulta = $.ajax({
+    return $.ajax({
         url: 'https://app-p2-js-c88e9128234a.herokuapp.com/mensagens',
         method: 'GET',
         dataType: 'json',
-        async: false
-    }).fail(function(){
-        return retorno;
+        crossDomain: true
     });
-
-    consulta.done(function(data) {
-        retorno = data;
-    });
-
-    return retorno;
 }
 
 function inserirMensagem(mensagem) {
-
-    /*
-
-    var mensagem = {
-            nome: "nome da pessoa", 
-            email: "email informado", 
-            mensagem: "a mensagem informada"} 
-
-    */
-
-    var inserir = $.ajax({
-
+    return $.ajax({
         url: 'https://app-p2-js-c88e9128234a.herokuapp.com/mensagens',
         method: 'POST',
         data: JSON.stringify(mensagem),
-        dataType: 'json',
-        async: false,
         contentType: 'application/json',
+        dataType: 'text',
+        crossDomain: true
     });
 }
 
 function validarUsuario(objLoginSenha) {
-
-    //email: admin@admin.com
-    //senha: '1234'
-
-    /*
-
-    var objLoginSenha = {
-            email: "email informado", 
-            senha: "senha informada"} 
-
-    */
-
-    var retorno = false;
-
-    var validacao = $.ajax({
+    return $.ajax({
         url: 'https://app-p2-js-c88e9128234a.herokuapp.com/usuarios/validar',
         method: 'POST',
-        dataType: 'json',
-        async: false,
-        headers: {
-            'Access-Control-Allow-Origin': '*'
-                },
         contentType: 'application/json',
+        dataType: 'text',
+        crossDomain: true,
         data: JSON.stringify(objLoginSenha)
-    }).fail(function(){
-        return retorno;
     });
-
-    validacao.done(function(data) {
-        retorno = data;
-    });
-
-    return retorno;
 }
